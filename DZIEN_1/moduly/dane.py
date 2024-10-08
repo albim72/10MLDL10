422ab181f1701f838e8edd5b50f6b7e2dbe16d3e
albim72 <marcin.albiniak72@gmail.com>
2024-10-08 11:18:22 +0200

    Create dane.py

---
+++ DZIEN_1/moduly/dane.py
@@ -0,0 +1,9 @@
+nrfilii = [5,24,6,9,15,16,25,57,78]
+
+book = {
+    "tytuł":"ABC biegacza górskiego",
+    "autor":"Marcin Świerc",
+    "cena":73,
+    "liczba stron":460,
+    "numer filii":nrfilii
+}
