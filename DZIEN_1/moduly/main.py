2d00a274a2e4c17ac86bf3568c124a3ec90987e6
albim72 <marcin.albiniak72@gmail.com>
2024-10-08 11:39:06 +0200

    Update main.py

--- DZIEN_1/moduly/main.py
+++ DZIEN_1/moduly/main.py
@@ -3,6 +3,14 @@
 from dane import nrfilii as nf
 from dane import book as bk
 
+from moje_funkcje.collectfunction import czytaj_liste, czytaj_slownik
+
 print("kolekcja z modułu: dane")
 print(nf)
 print(bk)
+
+print("kolekcje wyświetlane funkcjami:")
+print("_"*50)
+czytaj_liste(nf)
+print("_"*50)
+czytaj_slownik(bk)
