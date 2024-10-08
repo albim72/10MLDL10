0f65a605b70fe5cf4dd689795a8545732e0688bd
albim72 <marcin.albiniak72@gmail.com>
2024-10-08 11:38:39 +0200

    Update collectfunction.py

--- DZIEN_1/moduly/moje_funkcje/collectfunction.py
+++ DZIEN_1/moduly/moje_funkcje/collectfunction.py
@@ -1,7 +1,7 @@
 def czytaj_liste(lista):
     for i,j in enumerate(lista):
         print(f"numer indeksu elementu: {i}, wartość elementu: {j}")
-        
+
 def czytaj_slownik(slownik):
-    for x,y in slownik.item():
+    for x,y in slownik.items():
         print(f"element słownika: klucz -> {x}: wartość: {y}")
